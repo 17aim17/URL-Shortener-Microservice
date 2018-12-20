4fcfe53942b3286fcb09d524b774387b8913badd
Glitch (hello-express) <none>
2018-12-20 14:06:52 +0530

    working url shortner

--- server.js
+++ server.js
@@ -4,17 +4,52 @@
 // init project
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const validator = require('validator');
+const dns = require('dns');
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+app.use(bodyParser.json())
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
+
+
+let arr =[];
+
+// POST route for URL
+app.post('/api/shorturl/new', (req,res)=>{
+   const data = req.body.URL
+   if(validator.isURL(data)){
+      arr.push(data);
+      res.send({"original_url":data,"short_url":arr.length-1})
+   }else{
+     res.send({"error":"invalid URL"})
+   }
+
+})
+
+// To visit the URL
+app.get('/api/shorturl/:id', (req,res)=>{
+   const id =  req.params.id;
+   const link =arr[parseInt(id)];
+
+   res.redirect(addhttp(link))
+   
+})
+
+function addhttp(url) {
+  if (!/^(?:f|ht)tps?\:\/\//.test(url)) {
+      url = "http://" + url;
+  }
+  return url;
+}
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
