554fec2f84d77a70e3ff97611be657167e9f4175
Glitch (hello-express) <none>
2019-02-08 01:00:46 +0530

    better formatted data

--- server.js
+++ server.js
@@ -1,69 +1,73 @@
-// server.js
-// where your node app starts
-
-// init project
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv').config();
 const bodyParser = require('body-parser');
 const validator = require('validator');
-const crypto     = require('crypto')
-const { connect }= require('./db/connect/mongoose')
-const { Url } = require('./db/model/Model')
+const crypto = require('crypto');
+const { connect } = require('./db/connect/mongoose');
+const { Url } = require('./db/model/Model');
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+  response.sendFile(__dirname + 'index.html');
 });
 
-
 // POST route for URL
-app.post('/api/shorturl/new', (req,res)=>{
-   let data = req.body.URL
-   Url.findOne({originalUrl :data}).then(url=>{
-       if(url){
-          res.send({"original_url":url.originalUrl,"short_url":url.shortUrl})
-          return
-       }       
-       else if(validator.isURL(data)){
-          const sUrl =crypto.randomBytes(4).toString('hex')
-          const newData =new Url({
-            originalUrl: data,
-            shortUrl: sUrl
-          })
-          newData.save().then(url=>{
-            return res.send({"original_url":url.originalUrl,"short_url":url.shortUrl})
-          })
-       }else{
-         res.send({"error":"invalid URL"})
-       }
-      
-   }).catch(e=> console.log(e) )
+app.post('/api/shorturl/new', (req, res) => {
+  let data = req.body.URL;
 
-})
+  Url.findOne({ originalUrl: data })
+    .then(url => {
+      if (url) {
+        return res.status(200).json({
+          original_url: url.originalUrl,
+          short_url: `${req.protocol}://${req.headers.host}/api/shorturl/${
+            url.shortUrl
+          }`,
+          token: url.shortUrl
+        });
+      } else if (validator.isURL(data)) {
+        const sUrl = crypto.randomBytes(4).toString('hex');
+        const newData = new Url({
+          originalUrl: data,
+          shortUrl: sUrl,
+          token: url.shortUrl
+        });
+        newData.save().then(url => {
+          return res.status(201).json({
+            original_url: url.originalUrl,
+            short_url: `${req.protocol}://${req.headers.host}/api/shorturl/${
+              url.shortUrl
+            }`
+          });
+        });
+      } else {
+        res.status(400).json({ error: 'Invalid URL' });
+      }
+    })
+    .catch(e => console.log(e));
+});
 
 // To visit the URL
-app.get('/api/shorturl/:id', (req,res)=>{
-   const id =  req.params.id;
-   Url.findOne({shortUrl:id}).then(url=>{
-     return res.redirect(url.originalUrl)
-   }).catch(e=> res.send({error:'Invalid URL'}))
-    
-})
+app.get('/api/shorturl/:id', (req, res) => {
+  const id = req.params.id;
+  Url.findOne({ shortUrl: id })
+    .then(url => {
+      let link = url.originalUrl;
+      link = link.indexOf('://') === -1 ? 'http://' + link : link;
+      return res.redirect(link);
+    })
+    .catch(e => console.log(e));
+});
 
-app.get('*' ,(req,res)=>{
-  return res.send({msg :'You seem to be Lost'})
-})
+app.use((req, res, next) => {
+  return res.redirect('/');
+});
 
 // listen for requests :)
-const listener = app.listen(3000, function() {
+const listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
